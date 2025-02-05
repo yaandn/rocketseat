0a7342937de024f0d857616192a364b96f02dfd0
Yan Di Nápoli <dinapoliyan@gmail.com>
2025-02-05 09:48:15 -0300

    initi(project): create setup project

---
+++ curso-react/projects/pizza-shop/src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet-async";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1>dashboard</h1>
+    </>
+  );
+}
---
+++ curso-react/projects/pizza-shop/src/pages/auth/sign-in.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet-async";
+
+export function Signin() {
+  return (
+    <>
+      <Helmet title="Login" />
+      <h1>Signin</h1>
+    </>
+  );
+}
